d921b07a95de386d00379da025783e276e45f9b6
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2024-10-01 07:55:43 +0000

    compiti per casa

--- Informatica/Compiti per casa/01102024_2.c
+++ Informatica/Compiti per casa/01102024_2.c
@@ -3,7 +3,16 @@
 
 #include <stdio.h>
 int main(){
-    
+    float n1,n2,n3,max,min,media;
+    n1=0;
+    n2=0;
+    n3=0;
+    max=0;
+    min=0;
+    media=0;
+    printf("inserire i tre ivalori ");
+    scanf("%f%f%f",&n1,&n2,&n3);
+
 
 
 
