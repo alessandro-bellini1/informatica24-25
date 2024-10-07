85047259863c388199e60406b2f482d07c53c787
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2024-10-07 11:53:56 +0000

    numero somma divisione

---
+++ Informatica/Esercizi in Lab./07102024_3.c
@@ -0,0 +1,23 @@
+/*DATO UN NUMERO A PIU' CIFRE
+    ESEGUIRE LA SOMMA DELLE CIFRE E STABILIRE
+    SE E' DIVISIBILE PER 3*/
+#include <stdio.h>
+
+int main(){
+    int num=0;
+    int q, r;
+
+    printf("inserire un valore: ");
+        scanf("%d", &num);
+        q=num/10;
+        r=num%10;
+        if(r>0);
+
+
+        
+
+
+
+
+
+}
