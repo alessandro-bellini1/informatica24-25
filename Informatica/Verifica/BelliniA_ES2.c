5649f508b40a4eda623b5787d13e47889f92e93c
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2024-11-07 10:39:43 +0000

    verifica modifica

--- Informatica/Verifica/BelliniA_ES2.c
+++ Informatica/Verifica/BelliniA_ES2.c
@@ -3,9 +3,36 @@
 #include <stdio.h>
 int main(){
 
-int valore1, valore2, valore3, valore4;
-printf("inserire i 4 valori: ");
-scanf("%d,%d,%d,%d",&valore1,&valore2,&valore3,valore4);
-printf("%d,%d,%d,%d",valore1, valore2, valore3, valore4);
-
+    int valore1, valore2, valore3, valore4, somma;
+    float media;
+    printf("inserire il primo valore\nscelta: ");
+    scanf("%d",&valore1);
+    if(valore1>200)
+    printf("il primo valore supera 200");
+    else{
+        printf("inserire il secondo valore\nscelta: ");
+        scanf("%d",&valore2);
+        somma=valore1+valore2;
+        media=somma/2;
+        if(somma>200)
+        printf("i primi due valori sommati superano 200 e la loro media è: %.2f", media);
+        else{
+            printf("inserire il terzo valore\nscelta: ");
+            scanf("%d",&valore3);
+            somma=valore1+valore2+valore3;
+            media=somma/3;
+            if(somma>200)
+            printf("i primi 3 valori sommati superano 200 e la loro media è: %.2f", media);
+            else{
+                printf("inserire il quarto valore\nscelta: ");
+                scanf("%d",&valore4);
+                somma=valore1+valore2+valore3+valore4;
+                media=somma/4;
+                if(somma>200)
+                printf("i primi 4 valori sommati superano 200 e la loro media è: %.2f", media);
+                else
+                printf("i 4 valori sommati non superano 200");
+            }
+        }
+    }
 }
