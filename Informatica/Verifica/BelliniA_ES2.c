07d11ea4a322a60881020d2571d4c88070fb0078
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2024-11-11 10:46:58 +0000

    correzione verifica novembre

--- Informatica/Verifica/BelliniA_ES2.c
+++ Informatica/Verifica/BelliniA_ES2.c
@@ -1,38 +1,51 @@
 /*INSERIRE UNA SERIE DI VALORI FIN QUANDO LA LORO SOMMA NON SUPERA
 200, CALCOLARNE LA MEDIA E IL NUMERO DI VALORI INSERITI.*/
 #include <stdio.h>
-int main(){
 
-    int valore1, valore2, valore3, valore4, somma;
-    float media;
-    printf("inserire il primo valore\nscelta: ");
-    scanf("%d",&valore1);
-    if(valore1>200)
-    printf("il primo valore supera 200");
-    else{
-        printf("inserire il secondo valore\nscelta: ");
-        scanf("%d",&valore2);
-        somma=valore1+valore2;
-        media=somma/2;
-        if(somma>200)
-        printf("i primi due valori sommati superano 200 e la loro media è: %.2f", media);
-        else{
-            printf("inserire il terzo valore\nscelta: ");
-            scanf("%d",&valore3);
-            somma=valore1+valore2+valore3;
-            media=somma/3;
-            if(somma>200)
-            printf("i primi 3 valori sommati superano 200 e la loro media è: %.2f", media);
-            else{
-                printf("inserire il quarto valore\nscelta: ");
-                scanf("%d",&valore4);
-                somma=valore1+valore2+valore3+valore4;
-                media=somma/4;
-                if(somma>200)
-                printf("i primi 4 valori sommati superano 200 e la loro media è: %.2f", media);
-                else
-                printf("i 4 valori sommati non superano 200");
-            }
-        }
-    }
+int main(){
+	float numero, somma=0, media;
+	int cnt=0, flag=0;
+	
+	printf("Inserisci un valore: ");
+	scanf("%f", &numero);
+	somma=numero;
+	cnt++;
+	if(somma<200){
+		printf("Inserisci un valore: ");
+		scanf("%f", &numero);
+		somma+=numero;
+		cnt++;
+	}
+	else{
+		printf("Hai inserito il primo valore superiore a 200 e la media non è calcolabile.");
+		flag=1;
+		}
+	if(somma<200){
+		printf("Inserisci un valore: ");
+		scanf("%f", &numero);
+		somma+=numero;
+		cnt++;
+	}
+	else {if(flag!=1){
+			media=somma/cnt;
+			printf("\nHai inserito %d valori e la media vale %.2f ", cnt, media);
+			}
+		flag=1;
+		}
+	if(somma<200){
+		printf("Inserisci un valore: ");
+		scanf("%f", &numero);
+		somma+=numero;
+		cnt++;
+		media=somma/cnt;
+		printf("\nHai inserito %d valori e la media vale %.2f ", cnt, media);
+	}
+	else {if(flag!=1){
+			media=somma/cnt;
+			printf("\nHai inserito %d valori e la media vale %.2f ", cnt, media);
+			}
+		flag=1;
+		}
+				
+
 }
