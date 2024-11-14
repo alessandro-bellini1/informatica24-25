0a980c5fbb6f60cd1b7690969280cb5f73a7eec2
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2024-11-14 10:42:11 +0000

    ciclo n_2

---
+++ Informatica/Esercizi in Lab./CICLI/14112024_2.c
@@ -0,0 +1,18 @@
+/* LEGGERE UNA SEQUENZA DI NUMERI, VISUALIZZARLA.
+SI TERMINA NON APPENA ARRIVA UNO 0 */
+
+#include <stdio.h>
+
+int main(){
+    int num;
+
+    printf("Inserire un numero:\nscelta: ");
+    scanf("%d", &num);
+    
+    while(num|=0){
+        printf("Inserire un numero:\nscelta: ");
+        scanf("%d", &num);
+    }
+    printf("Hai inserito uno 0 il programma termina.");
+    return 0;
+}
