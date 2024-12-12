4b76ede5faa9d0cea9bd1dd4c135cb4ac54f278b
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2024-12-12 10:37:49 +0000

    modifica figura

--- Informatica/Esercizi in Lab./CICLI/FIGURA.c
+++ Informatica/Esercizi in Lab./CICLI/FIGURA.c
@@ -4,7 +4,7 @@
 int main(){
     for(int i=0; i<10; i++){
         for(int j=0; j<10; j++){
-            if(i==0 || i==0 || i==9-j){
+            if(i==0 || i==9 || i==9-j){
                 printf("*");
             } else{
                 printf(" ");
