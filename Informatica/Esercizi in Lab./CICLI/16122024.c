a3ef0e2867efca35ace0a45fb1eee04129f2875f
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2024-12-16 10:31:18 +0000

    es del 16 dicembre

---
+++ Informatica/Esercizi in Lab./CICLI/16122024.c
@@ -0,0 +1,15 @@
+/*Calcolare la somma dei primi N numeri naturali.
+Es: 5 1+2+3+4+5
+*/
+#include <stdio.h>
+int main(){
+    int N=0, somma=0;
+    do{printf("inserisci un numero\nscelta: ");
+    scanf("%d",&N);
+    }while(N<0);
+    for(int i=1; i<=N; i++){
+        somma+=i;
+        printf("%d+\n",i);
+    }
+    printf("la somma dei numeri N è: %d\n",somma);
+}
