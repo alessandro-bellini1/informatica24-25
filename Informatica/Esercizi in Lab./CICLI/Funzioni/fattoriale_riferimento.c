bad7440c72e00055159bae96f0e958a2a9241cce
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2025-02-06 10:21:15 +0000

    fattoriale_riferimento

--- Informatica/Esercizi in Lab./CICLI/Funzioni/fattoriale_riferimento.c
+++ Informatica/Esercizi in Lab./CICLI/Funzioni/fattoriale_riferimento.c
@@ -1,10 +1,11 @@
 /*calcolare il fattoriale di un numero*/
 #include <stdio.h>
 
-void fattoriale(int _n, int *_fatt);
+void fattoriale(int _n, long *_fatt);
 
 int main(){
-    int n, fatt=1;
+    int n;
+    long fatt=1;
     do{
         printf("inserisci un valore\nscelta: ");
         scanf("%d",&n);
@@ -15,7 +16,7 @@ int main(){
     return 0;
 }
 
-void fattoriale(int _n, int *_fatt){
+void fattoriale(int _n, long *_fatt){
     int i;
     for(i=1; i<=_n; i++)
         *_fatt*=i;
