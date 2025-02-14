f555dca69a19d965e281444d25bf09c7a9094269
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2025-02-14 21:48:48 +0000

    TRIS COMPLETATO

--- Informatica/Esercizi in Lab./CICLI/Funzioni/Tris.c
+++ Informatica/Esercizi in Lab./CICLI/Funzioni/Tris.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void SceltaSimbolo(char _x, char _o, char *_P1, char *_P2);
+int SceltaSimbolo(char _x, char _o, char *_P1, char *_P2);
 int gioco(char _simbolo_turno, char *_c1, char *_c2, char *_c3, char *_c4, char *_c5, char *_c6, char *_c7, char *_c8, char *_c9);
 void griglia(char *_c1, char *_c2, char *_c3, char *_c4, char *_c5, char *_c6, char *_c7, char *_c8, char *_c9);
 int vittoria(char _simbolo_turno, char _x, char _o, char _c1, char _c2, char _c3, char _c4, char _c5, char _c6, char _c7, char _c8, char _c9);
@@ -10,7 +10,8 @@ int main(){
     char x='X', o='O', simbolo_turno;
     char c1=' ',c2=' ',c3=' ',c4=' ',c5=' ',c6=' ',c7=' ',c8=' ',c9=' ';
     int turno=0;
-    SceltaSimbolo(x, o, &P1, &P2);
+    while(SceltaSimbolo(x, o, &P1, &P2)==1);
+   
 
     while(turno<9){
         if(turno%2==0)
@@ -24,17 +25,19 @@ int main(){
     if(gioco(simbolo_turno, &c1, &c2, &c3, &c4, &c5, &c6, &c7, &c8, &c9)==0)
         turno++;
     if(vittoria(simbolo_turno, x, o, c1, c2, c3, c4, c5, c6, c7, c8, c9)==1)
-        turno=9;
+        turno=10;
 
 }
-    
+if(turno==9)
+printf("PAREGGIO!!!\n");
+griglia(&c1, &c2, &c3, &c4, &c5, &c6, &c7, &c8, &c9);
 
 
 
 
 }
 
-void SceltaSimbolo(char _x, char _o, char *_P1, char *_P2){
+int SceltaSimbolo(char _x, char _o, char *_P1, char *_P2){
     printf("scegli il simbolo tra X e O\nScelta: ");
     scanf(" %c", &*_P1);
 
@@ -48,8 +51,10 @@ void SceltaSimbolo(char _x, char _o, char *_P1, char *_P2){
         break;
     default:
         printf("Opzione inesistenze.\n");
+        return 1;
         break;
     }
+    return 0;
 }
 
 void griglia(char *_c1, char *_c2, char *_c3, char *_c4, char *_c5, char *_c6, char *_c7, char *_c8, char *_c9){
@@ -129,54 +134,53 @@ int gioco(char _simbolo_turno, char *_c1, char *_c2, char *_c3, char *_c4, char 
 
 int vittoria(char _simbolo_turno, char _x, char _o, char _c1, char _c2, char _c3, char _c4, char _c5, char _c6, char _c7, char _c8, char _c9){
         if(_c1==_x&&_c1==_c2&&_c1==_c3){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c4==_x&&_c4==_c5&&_c4==_c6){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c7==_x&&_c7==_c8&&_c7==_c9){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c1==_x&&_c1==_c4&&_c1==_c7){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c2==_x&&_c2==_c5&&_c2==_c8){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c3==_x&&_c3==_c6&&_c3==_c9){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c1==_x&&_c1==_c5&&_c1==_c9){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c3==_x&&_c3==_c5&&_c3==_c7){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }
         else if(_c1==_o&&_c1==_c2&&_c1==_c3){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c4==_o&&_c4==_c5&&_c4==_c6){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c7==_o&&_c7==_c8&&_c7==_c9){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c1==_o&&_c1==_c4&&_c1==_c7){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c2==_o&&_c2==_c5&&_c2==_c8){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c3==_o&&_c3==_c6&&_c3==_c9){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c1==_o&&_c1==_c5&&_c1==_c9){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }else if(_c3==_o&&_c3==_c5&&_c3==_c7){
-            printf("il giocatore %c, HA VINTO!",_simbolo_turno);
+            printf("il giocatore %c, HA VINTO!\n",_simbolo_turno);
             return 1;
         }
-        
 }
