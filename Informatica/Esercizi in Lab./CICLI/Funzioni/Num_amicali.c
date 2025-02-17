eb13c604fa83bfc6e3fea8316b24597611cca4c8
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2025-02-17 10:52:47 +0000

    Num_amicali funzioni

---
+++ Informatica/Esercizi in Lab./CICLI/Funzioni/Num_amicali.c
@@ -0,0 +1,11 @@
+/*data una sequenza di valori, verificare se i valori presi a coppie risultano essere amicali, si termina quando uno dei valori è 0*/
+
+#include <stdio.h>
+int main(){
+    int n1, n2, somma1=0, somma2=0;
+    printf("inserisci un valore al primo numero\nscelta: ");
+    scanf("%d", &n1);
+    printf("inserisci un valore al secondo numero\nscelta: ");
+    scanf("%d", &n2);
+
+}
