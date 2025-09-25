bf41ade49d066c1d5438cbe1c66634bb2021d47e
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2025-09-25 10:15:21 +0000

    modifica

--- Informatica/Esercizi in Lab./funzioni_Malloc.c
+++ Informatica/Esercizi in Lab./funzioni_Malloc.c
@@ -7,61 +7,57 @@ IL PROGRAMMA DEVE PREVEDERE LESEGUENTI FUNZIONI
 #include <stdio.h>
 #include <stdlib.h>
 
-int* crea_array(int _dim);
-int* inserisci_elementi(int _dim);
-int stampa_elementi(int *array, int _dim);
-int libera_memoria(int **array);
+int* crea_array(int _dim); // funzione per creare un array dinamico
+void inserisci_elementi(int *_array, int _dim); // funzione per inserire elementi nell'array
+int stampa_elementi(int *array, int _dim); // funzione per stampare gli elementi dell'array
+int libera_memoria(int **array); // funzione per liberare la memoria allocata
 
 int main(){
-    int *array = NULL; //puntatore al vettore dinamico
-    int dim;
+    int *array = NULL; // puntatore al vettore dinamico
+    int dim; // variabile per la dimensione dell'array
 
     do{
-        printf("Inserisci la dimensione dell'array (maggiore di 0): \n");
-        scanf("%d", &dim);
-    } while (dim <= 0);
+        printf("Inserisci la dimensione dell'array (maggiore di 0): \n"); // richiesta dimensione
+        scanf("%d", &dim); // lettura dimensione
+    } while (dim <= 0); // controllo che sia maggiore di 0
 
+    array = crea_array(dim); // allocazione memoria per array
+    inserisci_elementi(array, dim); // inserimento elementi nell'array
+    stampa_elementi(array, dim); // stampa degli elementi
+    libera_memoria(&array); // liberazione memoria allocata
 
-    array = crea_array(dim);
-    array = inserisci_elementi(dim);
-    stampa_elementi(array, dim);
-    libera_memoria(&array);
-
-    return 0;
+    return 0; // termine programma
 }
 
 int* crea_array(int _dim){
-    int *_array = NULL;
-    _array = (int *)malloc(_dim * sizeof(int));
-    if(_array == NULL){
-        printf("Errore nell'allocazione della memoria\n");
-        exit(1);
+    int *_array = NULL; // dichiarazione puntatore array
+    _array = (int *)malloc(_dim * sizeof(int)); // allocazione memoria dinamica
+    if(_array == NULL){ // controllo allocazione
+        printf("Errore nell'allocazione della memoria\n"); // messaggio errore
+        exit(1); // uscita dal programma
     }
     return _array;
 }
 
-int* inserisci_elementi(int _dim){
-    int *_array = NULL;
-    _array = crea_array(_dim);
-    for(int i = 0; i < _dim; i++){
-        printf("Inserisci l'elemento %d: \n", i+1);
-        scanf("%d", &_array[i]);
+void inserisci_elementi(int *_array, int _dim){
+    for(int i = 0; i < _dim; i++){ // ciclo per inserimento elementi
+        printf("Inserisci l'elemento %d: \n", i+1); // richiesta elemento
+        scanf("%d", &_array[i]); // lettura elemento
     }
-    return _array;
 }
 
 int stampa_elementi(int *array, int _dim){
     printf("Gli elementi dell'array sono: \n");
-    for(int i = 0; i < _dim; i++){
-        printf("%d ", array[i]);
+    for(int i = 0; i < _dim; i++){ // ciclo per stampa elementi
+        printf("%d ", array[i]); // stampa elemento
     }
     printf("\n");
     return 0;
 }
 
 int libera_memoria(int **array){
-    free(*array);
-    *array = NULL;
+    free(*array); // liberazione memoria
+    *array = NULL; // azzeramento puntatore
     return 0;
 }
 
