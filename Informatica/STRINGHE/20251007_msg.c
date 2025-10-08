714a46d78a667012d0e8249cb356cce1374d4110
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2025-10-08 17:41:07 +0000

    CREAZIONE STRINGHE

---
+++ Informatica/STRINGHE/20251007_msg.c
@@ -0,0 +1,35 @@
+/*Scrivi un programma in C che permetta di far scrivere un messaggio 
+di lunghezza variabile all'utente.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char* String;
+
+int main(){
+    String frase = NULL;
+    int cnt = 0;
+    int capacita = 0;
+    char c;
+    
+    for(c = getchar(); c != '\n'; c = getchar()) {
+        if(cnt + 1 > capacita){
+            capacita += 10;
+            frase = (String) realloc(frase, capacita * sizeof(char));
+            if(frase == NULL){
+                printf("Ahia ahia :(");
+                return 1;
+            }
+        }      
+        
+        frase[cnt] = c;
+        cnt++;
+    }
+    frase[cnt] = '\0';
+    
+    printf("%s", frase);
+
+    free(frase);
+
+    return 0;
+}
