c7bf45fe7cd5abc44019cf62bc9ef9b8b3d00f0d
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2025-10-21 07:35:34 +0000

    conta vocali

---
+++ Informatica/STRINGHE/str_malloc.c
@@ -0,0 +1,47 @@
+/*data una stringa contare il numero delle vocali.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef char* Stringa;
+
+void compatta(Stringa buffer, int len){
+    if(len>0 && buffer[len-1]=='\n'){
+        buffer[len-1] = '\0';
+        len--;
+    }
+}
+
+int conta_vocali(Stringa s){
+    int cnt = 0;
+    for(int i = 0; s[i] != '\0'; i++){
+        char c = s[i];
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+           c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    char buffer[200];
+    Stringa frase;
+
+    printf("Inserisci una frase: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    int len = strlen(buffer);
+    compatta(buffer, len);
+    frase = (Stringa)malloc((len + 1) * sizeof(char));
+    if (frase == NULL) {
+        printf("Errore di allocazione!\n");
+        return 1;
+    }
+    strcpy(frase, buffer);
+
+    printf("il numero delle vocali e': %d\n", conta_vocali(frase));
+    free(frase);
+    return 0;
+}
+
