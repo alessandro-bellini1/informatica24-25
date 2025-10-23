32a02bed2b72c84a91b6cb91e060654a1c38f8f7
alessandro-bellini1 <alessandro.bellini@galileiostiglia.edu.it>
2025-10-23 11:37:02 +0000

    palindroma

---
+++ Informatica/STRINGHE/palindroma.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "libstring.h"
+#include "libstring.c"
+
+void palindroma(Stringa s, int _dim){
+    Bool flag = true;
+    for(int i = 0; i < _dim / 2; i++){
+        if(s[i] != s[_dim - i - 1]){
+            flag = false;
+            break;
+        }
+    }
+    if(flag)
+        printf("La stringa è palindroma\n");
+    else
+        printf("La stringa non è palindroma\n");
+
+}
+
+int main(){
+    Stringa s1 = crea_stringa_dinamica('\n');
+    int dim = calcola_lunghezza_stringa(s1);
+
+    palindroma(s1, dim);
+}
